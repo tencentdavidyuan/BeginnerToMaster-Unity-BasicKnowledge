23872132e160550eee79d02f86fdef57fe9864ef
davidyuan <148389098@qq.com>
2020-09-25 14:12:45 +0800

    添加CatLike Code学习

---
+++ Assets/18 CatLike Coding Hex Map/1. Creating a Hexagonal Grid/HexGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int _width;
+    public int _height;
+
+    public HexCell _hexCellPefab;
+    public HexCell[] _hexCells;
+
+    #region Unity Methods
+    private void Awake() {
+        _hexCells = new HexCell[_width * _height];
+
+        for (int z = 0, count = 0; z < _height; z++) {
+            for (int x = 0; x < _width; x++) {
+                CreateHexCell(x, z, count);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    #endregion
+
+    #region Private Methods
+    private void CreateHexCell(int x, int z, int count) {
+        Vector3 position;
+        
+    }
+    #endregion
+
+}
---
+++ Assets/18 CatLike Coding Hex Map/1. Creating a Hexagonal Grid/HexMetrics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetics
+{
+    public const float _outerRadius = 10f;
+    public const float _innerRadius = _outerRadius * 0.866025404f;
+
+    public static Vector3[] _conners = {
+        new Vector3(0f, 0f, _outerRadius),
+        new Vector3(_innerRadius, 0f, _outerRadius *  0.5f),
+        new Vector3(_innerRadius, 0f, _outerRadius * -0.5f),
+        new Vector3(0f, 0f, -_outerRadius),
+        new Vector3(-_innerRadius, 0f, _outerRadius * -0.5f),
+        new Vector3(-_innerRadius, 0f, _outerRadius *  0.5f),
+    };
+}
